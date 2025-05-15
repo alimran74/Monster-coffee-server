af5ea02b9ecc64b1be616158b5ea199c5e427dce
alimran74 <alimranimran748596@gmail.com>
2025-05-16 00:59:12 +0600

    added put delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -39,7 +39,14 @@ async function run() {
 
         const result = await coffeesCollection.find().toArray();
         res.send(result);
-    })
+    });
+
+    app.get('/coffees/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.findOne(query);
+      res.send(result)
+    }) 
 
     app.post('/coffees', async(req, res) => {
         const newCoffee = req.body;
@@ -47,6 +54,34 @@ async function run() {
         const result = await coffeesCollection.insertOne(newCoffee);
         res.send(result);
     })
+
+
+    app.put('/coffees/:id', async(req, res) =>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)};
+      const options = {upsert: true };
+      const updatedCoffee = req.body;
+      const updatedDoc = {
+          $set: updatedCoffee
+      }
+
+      const result = await coffeesCollection.updateOne(filter, updatedDoc, options);
+
+      res.send(result)
+
+
+    })
+
+    app.delete('/coffees/:id', async (req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await coffeesCollection.deleteOne(query);
+      res.send(result);
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
